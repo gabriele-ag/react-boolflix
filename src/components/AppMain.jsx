490eacb6f395a8976b70ce9649ab627a0fe27b56
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-06-17 13:22:17 +0200

    feat: search only if click button

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -5,42 +5,55 @@ import axios from "axios"
 function Main() {
     const [movie, setMovie] = useState([])
     const [search, setSearch] = useState("")
+    const [query, setQuery] = useState("")
+    const [error, setError] = useState(false)
 
     const apiKey="05f25a38e80d3f281215e0480061a071"
     const apiUrl="https://api.themoviedb.org/3/search/movie"
 
     useEffect(() => {
             axios
-                .get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`)
+                .get(`${apiUrl}?api_key=${apiKey}&query=${query}`)
                 .then((resp) => {
-                const movieList = resp.data
+                const movieList = resp.data.results
                 console.log(movieList)
                 setMovie(movieList)
                 })
-    }, [])
+    }, [query])
 
     const handleInput = (event) => {
-        event.preventDefault()
         setSearch(event.target.value)
+    }
+
+    const handleSearch = () => {
+        if (search) {
+            setQuery(search)
+        }
     }
 
 
 
     return (
         <main>
-            <form onSubmit={handleInput}>
+
                 <label>Cerca il tuo film</label>
                 <input
                 type=""
                 value={search}
-                onChange={(event) => setSearch(event.target.value)}
+                onChange={handleInput}
                 placeholder="Search Movie"
                 ></input>
-                <button type="submit">Cerca</button>
-            </form>
-            <div>
-                {movie.map((curFilm))}
-            </div>
+                <button type="submit" onClick={handleSearch}>Cerca</button>
+
+            
+                <div>
+                    {movie.map((curMovie, index) => (
+                        <div key={index}>
+                            <h2>Nome del film: {curMovie.title}</h2>
+                        </div>
+                    ))}
+                </div>
+            
         </main>
     )
 
